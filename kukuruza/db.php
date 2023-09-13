bf7a8f8ae3c1e2bf81feebb0e47b2719a82b1350
SSapeRR <93428308+ssaperr@users.noreply.github.com>
2023-09-13 04:17:20 +0300

    Добавлено упоминание об авторе библ. SafeMySql

--- kukuruza/db.php
+++ kukuruza/db.php
@@ -1,4 +1,19 @@
 <?php
+/*
+Copyright 2023 col.shrapnel@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 /**
  * @author col.shrapnel@gmail.com
  * @link http://phpfaq.ru/safemysql
@@ -77,10 +92,10 @@ class SafeMySQL
 	protected $exname;
 
 	protected $defaults = array(
-		'host'      => 'localhost',
-		'user'      => 'etubot',
-		'pass'      => 'JECRXmCfikcztkwc',
-		'db'        => 'etubot',
+		'host'      => '',
+		'user'      => '',
+		'pass'      => '',
+		'db'        => '',
 		'port'      => NULL,
 		'socket'    => NULL,
 		'pconnect'  => FALSE,
